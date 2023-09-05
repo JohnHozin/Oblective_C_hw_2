834926818dfcf34403315b064932f2875331bdf3
john <sim@mail.ru>
2023-09-05 06:26:32 -0700

    Initial Commit

---
+++ Oblective_C_hw_2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Oblective_C_hw_2
+//
+//  Created by умпет on 05.09.2023.
+//  Copyright © 2023 Evgen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
